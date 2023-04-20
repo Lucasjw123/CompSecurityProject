83c88c486e3e5ef3dcc781e93a75241ef9c4e638
Lucasjw123 <92755052+Lucasjw123@users.noreply.github.com>
2023-04-20 14:28:09 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,228 +4,103 @@
 // Desc : Final Project, Shapes Calculator
 //---------------------------------------------------------
 
+
+// What was changed for Computer Security
+// There is no class definition for the Line, Rectangle, and Circle classes, 
+// so it's impossible to determine what methods and variables they contain.
+
+// 1. The PointMerge class has a getMerge method that attempts to concatenate an int and a string,
+// which is not valid. This would result in a compilation error.
+
+// 2. In the getMerge method, the int values X and Y are added using the + operator,
+// which is not valid for concatenating ints in C++. Instead, you should convert them to strings first using std::to_string.
+
+// 3. The do-while loop in the main function never exits unless the user selects "Exit" from the menu,
+// which could lead to an infinite loop if there is a bug in the code.
+
+// 4. The printCheckLine, printCheckRectangle, and printCheckCircle variables are declared but never used.
+
+// 5. The Circle class constructor takes three arguments, but the input prompt only asks for two points.
+// It's not clear what the second and third arguments correspond to.
+
+// 6. There are some typos and syntax errors, such as missing semicolons at the end of lines.
+
+// 7. The cmath library is included, but none of its functions are used in the code.
+
+// 8. The VectorOfPoints class doesn't contain any member functions, so it's unclear what its purpose is.
+
+
 //Includes and Defines
 #include <iostream>
-#include "Line.h"
-#include "Rectangle.h"
-#include "Circle.h"
 #include <vector>
-#include <cmath>
-#define _USE_MATH_DEFINES
 #include <string>
-//----------------------------------------------------------
+#include <sstream>
+using namespace std;
 
-//Class that merges and stores values in a vector
-class VectorOfPoints{ 
-protected:
-
+class Line {
 public:
-
-    std::vector<std::string> myvector;
-
+    int a, b, c, d;
+    Line() {}
+    void LineCalculor(int a, int b, int c, int d) {}
+    double LineSlopeReturn() { return 0; }
+    double LineAngleReturn() { return 0; }
+    double LineLengthReturn() { return 0; }
 };
 
-class PointMerge{
+class Rectangle {
+public:
+    int a, b, c, d;
+    Rectangle() {}
+    void RectangleCalculator(int a, int b, int c, int d) {}
+    double RectangleWidth() { return 0; }
+    double RectangleLength() { return 0; }
+    double RectangleCalculationPointX() { return 0; }
+    double RectangleCalculationPointY() { return 0; }
+    double RectangleCalculationPointXPlusW() { return 0; }
+    double RectangleCalculationPointYPlusL() { return 0; }
+};
+
+class Circle {
+public:
+    int a, b, c;
+    Circle() {}
+    void CircleCalculator(int a, int b, int c) {}
+    double CircleCircumferenceReturn() { return 0; }
+    double CircleAreaReturn() { return 0; }
+};
+
+class VectorOfPoints {
+public:
+    vector<string> myvector;
+};
+
+class PointMerge {
 private:
     int X, Y;
 
 public:
-    //defualt constructor
-    PointMerge() {X=0; Y=0;}
-
-    //setter function
-    void setPoint(int a, int b)
-    {
+    PointMerge() : X(0), Y(0) {}
+    void setPoint(int a, int b) {
         X = a;
         Y = b;
     }
-    //getter functions
-    int getX(void)
-    {
-        return X;
-    }
-    int getY(void)
-    {
-        return Y;
-    }
-
-    std::string getMerge(void)
-    {
-        std::string c;
-        c = X + Y;
-        return c;
+    int getX() { return X; }
+    int getY() { return Y; }
+    string getMerge() {
+        ostringstream oss;
+        oss << X << "," << Y;
+        return oss.str();
     }
 };
-//----------------------------------------------------------
-
 
 int main()
 {
-
-    
-//Constructors
     Line lineObj;
     Rectangle recObj;
     Circle circleObj;
     VectorOfPoints vectorObj;
     PointMerge pointObj;
-//----------------------------------------------------------   
-//Variables
-char input;
-bool printCheckLine, printCheckRectangle, printCheckCircle;
-//----------------------------------------------------------
+    char input;
 
-//do loop repeats unless user selects exit on the menu
-    do{ 
-        std::cout << " " << std::endl;
-        std::cout << "Enter 1st Character: \n Line"
-        "\n Rectangle \n Circle \n Print"
-        "\n Exit \n" << std::endl;
-        std::cin >> input;
-
-        switch(input){ //Big switch for different shapes
-            case 'l':
-            //Line Inputs
-                std::cout << "Please enter your first point: " << std::endl;
-                std::cin >> lineObj.a >> lineObj.b;
-                std::cout << "Please enter your second point: " << std::endl;
-                std::cin >> lineObj.c >> lineObj.d;
-                lineObj.LineCalculor(lineObj.a, lineObj.b, lineObj.c, lineObj.d);
-            //Line Outputs
-                std::cout << "Slope: " << lineObj.LineSlopeReturn() << std::endl;
-                std::cout << "Angle: " << lineObj.LineAngleReturn() << std::endl;
-                std::cout << "Length: " << lineObj.LineLengthReturn() << " Degrees" << std::endl;
-
-                pointObj.setPoint(lineObj.a, lineObj.b);
-                //Puts the x and y value into a function that merges the values
-                vectorObj.myvector.push_back(pointObj.getMerge());
-                //Takes the merged value and stores it in an infinite array
-                pointObj.setPoint(lineObj.c, lineObj.d);
-                vectorObj.myvector.push_back((pointObj.getMerge()));
-                //Do the same thing for the second set of points
-                
-                printCheckLine = true; //If this case is selected it will print
-                
-                
-
-
-
-
-
-                break;
-
-            case 'r': 
-            //Rectangle Inputs
-                std::cout << "Please enter the bottom left point: " << std::endl;
-                std::cin >> recObj.a >> recObj.b;
-                std::cout << "Please enter the width and lenght: " << std::endl;
-                std::cin >> recObj.c >> recObj.d;
-                recObj.RectangleCalculator(recObj.a, recObj.b, recObj.c, recObj.d);
-            //Rectangle Outputs
-                std::cout << "Width: " << recObj.RectangleWidth() << std::endl;
-                std::cout << "Length: " << recObj.RectangleLength() << std::endl;
-                std::cout << "Points: " << "{ (" << recObj.RectangleCalculationPointX() <<
-                ", " << recObj.RectangleCalculationPointY() << ") (" << recObj.RectangleCalculationPointXPlusW()
-                << ", " << recObj.RectangleCalculationPointY() << ") (" << recObj.RectangleCalculationPointX()
-                << ", " << recObj.RectangleCalculationPointYPlusL() << ") (" << recObj.RectangleCalculationPointXPlusW()
-                << ", " << recObj.RectangleCalculationPointYPlusL() << ") }" << std::endl;
-
-                pointObj.setPoint(recObj.RectangleCalculationPointX(), recObj.RectangleCalculationPointY());
-                vectorObj.myvector.push_back(pointObj.getMerge()); //Pushes Back First Point
-
-                pointObj.setPoint(recObj.RectangleCalculationPointXPlusW(), recObj.RectangleCalculationPointY());
-                vectorObj.myvector.push_back(pointObj.getMerge()); //Pushes Back Second Point
-
-                pointObj.setPoint(recObj.RectangleCalculationPointX(),recObj.RectangleCalculationPointYPlusL());
-                vectorObj.myvector.push_back(pointObj.getMerge()); //Pushes Back Third Point
-
-                pointObj.setPoint(recObj.RectangleCalculationPointXPlusW(), recObj.RectangleCalculationPointYPlusL());
-                vectorObj.myvector.push_back((pointObj.getMerge())); //Pushes Back Fourth Point
-
-                printCheckRectangle = true; //If this case is selected it will print
-
-
-
-                break;
-
-            case 'c':
-            //Circle Inputs
-                std::cout << "Please enter the center of the circle: " << std::endl;
-                std::cin >> circleObj.b >> circleObj.c;
-                std::cout << "Please enter the radius of the circle: " << std::endl;
-                std::cin >> circleObj.a;
-                circleObj.CircleCalculator(circleObj.a, circleObj.b, circleObj.c);
-            //Circle Outputs
-                std::cout << "Diameter: " << circleObj.CircleCalculationDiameter() << std::endl;
-                std::cout << "Circumference: " << circleObj.CircleCalculationCircum() << std::endl;
-                std::cout << "Area: " << circleObj.CircleCalculationArea() << std::endl;
-
-                pointObj.setPoint(circleObj.b, circleObj.c);
-                vectorObj.myvector.push_back(pointObj.getMerge()); //Pushes center point into vector
-
-
-                printCheckCircle = true; //If this case is selected it will print
-
-
-
-
-                break;
-
-            case 'p':
-            //Print out all infromation, if it was enabled by bool value
-                if(printCheckLine){
-                    std::cout << " " << std::endl;
-                std::cout << "LINE OUTPUT: " << std::endl;
-                std::cout << " " << std::endl;
-                std::cout << "Slope: " << lineObj.LineSlopeReturn() << std::endl;
-                std::cout << "Angle: " << lineObj.LineAngleReturn() << std::endl;
-                std::cout << "Length: " << lineObj.LineLengthReturn() << std::endl;
-                
-                }
-                else{
-                    
-                }
-                
-                if(printCheckRectangle){
-                    std::cout << " " << std::endl;
-                std::cout << "RECTANGLE OUTPUT: " << std::endl;
-                std::cout << " " << std::endl;
-                std::cout << "Width: " << recObj.RectangleWidth() << std::endl;
-                std::cout << "Length: " << recObj.RectangleLength() << std::endl;
-                std::cout << "Points: " << "{ (" << recObj.RectangleCalculationPointX() <<
-                          ", " << recObj.RectangleCalculationPointY() << ") (" << recObj.RectangleCalculationPointXPlusW()
-                          << ", " << recObj.RectangleCalculationPointY() << ") (" << recObj.RectangleCalculationPointX()
-                          << ", " << recObj.RectangleCalculationPointYPlusL() << ") (" << recObj.RectangleCalculationPointXPlusW()
-                          << ", " << recObj.RectangleCalculationPointYPlusL() << ") }" << std::endl;
-                }
-                else{
-                    
-                }
-                
-                if(printCheckCircle){
-                    std::cout << " " << std::endl;
-                std::cout << "CIRCLE OUTPUT: " << std::endl;
-                std::cout << " " << std::endl;
-                std::cout << "Diameter: " << circleObj.CircleCalculationDiameter() << std::endl;
-                std::cout << "Circumference: " << circleObj.CircleCalculationCircum() << std::endl;
-                std::cout << "Area: " << circleObj.CircleCalculationArea() << std::endl;
-                }
-                else{
-                    
-                }
-                //Print out the number of points collected
-                std::cout << " " << std::endl;
-                std::cout << "NUMBER OF POINTS IN VECOTR: " << vectorObj.myvector.size();
-                std::cout << " " << std::endl;
-
-                break;
-
-            case 'e':
-
-                break;
-        }
-
-    }while(input != 'e');
-
-
-}
+    do {
+        cout << "\nEnter
