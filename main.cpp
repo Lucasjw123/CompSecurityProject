f062283f18fbee7d18397337a0be6a580d467fac
Lucasjw123 <92755052+Lucasjw123@users.noreply.github.com>
2023-04-20 14:56:40 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,29 +5,33 @@
 //---------------------------------------------------------
 
 
-// What was changed for Computer Security
-// There is no class definition for the Line, Rectangle, and Circle classes, 
+// What was changed for Computer Security:
+
+// 1. There is no class definition for the Line, Rectangle, and Circle classes, 
 // so it's impossible to determine what methods and variables they contain.
 
-// 1. The PointMerge class has a getMerge method that attempts to concatenate an int and a string,
+// 2. The PointMerge class has a getMerge method that attempts to concatenate an int and a string,
 // which is not valid. This would result in a compilation error.
 
-// 2. In the getMerge method, the int values X and Y are added using the + operator,
+// 3. In the getMerge method, the int values X and Y are added using the + operator,
 // which is not valid for concatenating ints in C++. Instead, you should convert them to strings first using std::to_string.
 
-// 3. The do-while loop in the main function never exits unless the user selects "Exit" from the menu,
+// 4. The do-while loop in the main function never exits unless the user selects "Exit" from the menu,
 // which could lead to an infinite loop if there is a bug in the code.
 
-// 4. The printCheckLine, printCheckRectangle, and printCheckCircle variables are declared but never used.
+// 5. The printCheckLine, printCheckRectangle, and printCheckCircle variables are declared but never used.
 
-// 5. The Circle class constructor takes three arguments, but the input prompt only asks for two points.
+// 6. The Circle class constructor takes three arguments, but the input prompt only asks for two points.
 // It's not clear what the second and third arguments correspond to.
 
-// 6. There are some typos and syntax errors, such as missing semicolons at the end of lines.
+// 7. There are some typos and syntax errors, such as missing semicolons at the end of lines.
 
-// 7. The cmath library is included, but none of its functions are used in the code.
+// 8. The cmath library is included, but none of its functions are used in the code.
 
-// 8. The VectorOfPoints class doesn't contain any member functions, so it's unclear what its purpose is.
+// 9. The VectorOfPoints class doesn't contain any member functions, so it's unclear what its purpose is.
+
+// it is important to note that code quality issues can indirectly impact security. 
+// Poorly written code can contain vulnerabilities that can be exploited by attackers.
 
 
 //Includes and Defines
